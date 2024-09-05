d0e0c856886a820086a24cb11a5d74fffb7d767f
Karel <karelh2207@gmail.com>
2024-09-05 12:34:29 -0300

    actualizando

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,63 @@
-import { Box } from "@mui/material"
+import { Box, Button, TextField } from "@mui/material";
+
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  // Handle form submission logic here
+  console.log("Form submitted");
+  e.preventDefault();
+};
 
 const Login = () => {
   return (
     <>
       <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
           marginTop: "64px", // 64px cada barra
-        }}>Login</Box>
+        }}>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{
+            "& > :not(style)": { m: 1, width: "60%" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            required
+            fullWidth
+            helperText="Requerido"
+            error={false}
+          />
+          <TextField
+            id="filled-basic"
+            label="Filled"
+            variant="filled"
+            required
+            fullWidth
+            helperText="Requerido"
+            error={false}
+          />
+          <TextField
+            id="standard-basic"
+            label="Standard"
+            variant="standard"
+            required
+            fullWidth
+            helperText="Requerido"
+            error={false}
+          />
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <Button type="submit" sx={{mr: '5%'}} >Register</Button>
+        </Box>
+      </Box>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
